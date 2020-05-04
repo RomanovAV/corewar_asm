0c5ccfce20b88a641899abba2e74d617ff9fa8bc
Ilya <makarumba.ilya@gmail.com>
2020-05-04 13:21:30 +0300

    prototype of main

--- src/asm.c
+++ src/asm.c
@@ -1,6 +1,23 @@
 #include "asm.h"
 
-int main()
+int g_cur_line;
+int g_cur_col;
+
+int main(int ac, char **av)
 {
+	int	flag;
+	int	index;
+
+	if (ac == 1)
+		faq();
+	index = 1;
+	while (index < ac)
+	{
+		flag = av[1][0] == "-" ? get_flag(av[index++]) : 0;
+		if (index == ac)
+			error("file not specified", 0);
+		pricess(av[index], flag);
+		++index;
+	}
 	return (0);
 }
